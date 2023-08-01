cd095836750ac62bd77d38ab5234176f019814c5
Sudipta Basak <sudiptab2100@gmail.com>
2023-08-01 09:26:16 +0530

    removed unnecessary import

--- shop/src/main/kotlin/com/micro/shop/OrderProducer.kt
+++ shop/src/main/kotlin/com/micro/shop/OrderProducer.kt
@@ -3,7 +3,6 @@ package com.micro.shop
 import io.micronaut.configuration.kafka.annotation.KafkaClient
 import io.micronaut.configuration.kafka.annotation.KafkaKey
 import io.micronaut.configuration.kafka.annotation.Topic
-import java.math.BigDecimal
 
 @KafkaClient
 interface OrderProducer {
